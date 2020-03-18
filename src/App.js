9a16710e92b0a7ba551d8f18afa4a3498f349258
hungprok <57722641+hungprok@users.noreply.github.com>
2020-03-19 06:31:00 +0700

    final

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import React, { Component } from "react";
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import AwesomeComponent from "./Component/AwesomeComponent";
+// import { Container } from "react-bootstrap/lib/Tab";
+// import { Row } from "react-bootstrap";
+// import { Col } from "react-bootstrap";
 
 // function Render() {
 //   let [weather, setWeather] =useState(null);
@@ -63,7 +66,7 @@ class App extends Component {
       }
       else {
         alert("API is not correct, please wait a second or refresh the page!");
-        throw new Error("API is not correct, please wait a second!"); 
+        throw new Error("API is not correct, please wait a second!");
       }
     } catch (error) {
       console.log(error);
@@ -82,24 +85,50 @@ class App extends Component {
 
   render() {
     if (this.state.weather === null) {
-      return (
-        <div>
-          <AwesomeComponent />
-        </div>)
+      return (<div>
+        <div className="container">
+          <div className="row">
+            <div className="col-sm"></div>
+            <div className="col-sm text-center"><AwesomeComponent />
+            </div>
+            <div className="col-sm"></div>
+          </div>
+        </div>
+      </div>)
     }
 
     return (
       <div>
-        <div className="container-fluid text-white my-auto body">
-          <div className="container mx-auto my-4 py-4">
+        <div className="container-fluid body text-monospace">
+          <div className="container">
             <div className="row justify-content-center text-center">
-              <h1 className="col-12 display-4 my-2 py-3 text-success">
-                Awesome Weather App
-                 </h1>
-              <h2 className="col-12">{this.state.weather.name}</h2>
-              <h3 className="col-12 text-danger">{this.state.weather.main.temp}℃</h3>
-              <h3 className="col-12">{this.state.weather.weather[0].description}</h3>
-<div></div>
+              <h4 className="col-12 display-4 my-2 py-3 text-white">
+                <img height="100px" src="https://api.elementalweather.com/static/images/icon/night/bkn.gif" alt="" />
+                HOW'S THE WEATHER?
+                 </h4>
+              <div className="row">
+                <div className="col-sm-2"></div>
+                <div className="col-md-8 text-center justify-content-center">
+                  <div className="card" style={{ width: '100%' }}>
+                    <img src="https://www.tripsavvy.com/thmb/pePcQqX77-Ms6xcJ7YVzfgMmz3c=/3707x2085/smart/filters:no_upscale()/saigon-ho-chi-minh-city-vietnam-5c489f50c9e77c000112d22b.jpg" className="card-img-top" alt="..." />
+                    <div className="card-body">
+                      <h1 className="card-title">{this.state.weather.name}</h1>
+                      <h3 className="card-text text-muted">{this.state.weather.weather[0].description}</h3>
+                      <h1 className="card-title">{this.state.weather.main.temp}℃</h1>
+                    </div>
+                    {/* <ul className="list-group list-group-flush">
+                      <li className="list-group-item">Cras justo odio</li>
+                      <li className="list-group-item">Dapibus ac facilisis in</li>
+                      <li className="list-group-item">Vestibulum at eros</li>
+                    </ul> */}
+                    {/* <div className="card-body">
+                      <a href="#" className="card-link">Card link</a>
+                      <a href="#" className="card-link">Another link</a>
+                    </div> */}
+                  </div>
+                </div>
+                <div className="col-sm-2"></div>
+              </div>
             </div>
           </div>
         </div>
