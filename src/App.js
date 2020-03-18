81b56dd27f9cb95b2a9154e886ce148efdd44db5
hungprok <57722641+hungprok@users.noreply.github.com>
2020-03-19 05:11:45 +0700

    second

--- src/App.js
+++ src/App.js
@@ -97,7 +97,7 @@ class App extends Component {
                 Awesome Weather App
                  </h1>
               <h2 className="col-12">{this.state.weather.name}</h2>
-              <h3 className="col-12 text-danger">{this.state.weather.main.temp}</h3>
+              <h3 className="col-12 text-danger">{this.state.weather.main.temp}℃</h3>
               <h3 className="col-12">{this.state.weather.weather[0].description}</h3>
 <div></div>
             </div>
